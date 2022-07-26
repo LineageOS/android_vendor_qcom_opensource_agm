f725c4ff80ff75bcf86a22c95af41d00c91f3fda
Dhanalakshmi Siddani <quic_dsiddani@quicinc.com>
2022-07-26 19:00:13 +0530

    plugins: Use vendor specific tinycompress project
    Use vendor specific tinycompress projects based on compiler flag
    
    Change-Id: I31904b807e73cf82e039fa50c131dd7cdcbff386

--- plugins/tinyalsa/test/Android.mk
+++ plugins/tinyalsa/test/Android.mk
@@ -134,9 +134,9 @@ LOCAL_HEADER_LIBRARIES := \
     libagm_headers \
     libacdb_headers
 
-#if android version is R, refer to qtitinyxx otherwise use upstream ones
-#This assumes we would be using AR code only for Android R and subsequent versions.
-ifneq ($(filter 11 R, $(PLATFORM_VERSION)),)
+# Use flag based selection to use QTI vs open source tinycompress project
+
+ifeq ($(TARGET_USES_QTI_TINYCOMPRESS),true)
 LOCAL_C_INCLUDES += $(TOP)/vendor/qcom/opensource/tinyalsa/include
 LOCAL_C_INCLUDES += $(TOP)/vendor/qcom/opensource/tinycompress/include
 LOCAL_SHARED_LIBRARIES += libqti-tinyalsa\
@@ -171,9 +171,9 @@ LOCAL_HEADER_LIBRARIES := \
     libagm_headers \
     libacdb_headers
 
-#if android version is R, refer to qtitinyxx otherwise use upstream ones
-#This assumes we would be using AR code only for Android R and subsequent versions.
-ifneq ($(filter 11 R, $(PLATFORM_VERSION)),)
+# Use flag based selection to use QTI vs open source tinycompress project
+
+ifeq ($(TARGET_USES_QTI_TINYCOMPRESS),true)
 LOCAL_C_INCLUDES += $(TOP)/vendor/qcom/opensource/tinyalsa/include
 LOCAL_C_INCLUDES += $(TOP)/vendor/qcom/opensource/tinycompress/include
 LOCAL_SHARED_LIBRARIES += libqti-tinyalsa\
