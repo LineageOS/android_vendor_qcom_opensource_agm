58c1c901810528802a10560399d6bc20d8bb29ce
Harold Tan <quic_harotan@quicinc.com>
2022-09-26 11:18:46 -0700

    agm: remove "LOG_TAG" in device.c
    Remove "LOG_TAG" in AGM_LOG statements in
    wait_for_snd_card_to_online() for working
    condition of AGM log printing.
    
    Change-Id: Ife34de7a0a3817602bcc7961315b8ae8876f33e4

--- service/src/device.c
+++ service/src/device.c
@@ -1046,7 +1046,7 @@ static int wait_for_snd_card_to_online()
     /* maximum wait period = (MAX_RETRY * RETRY_INTERVAL_US) micro-seconds */
     do {
         if ((fd = open(SNDCARD_PATH, O_RDWR)) < 0) {
-            AGM_LOGE(LOG_TAG, "Failed to open snd sysfs node, will retry for %d times ...", (retries - 1));
+            AGM_LOGE("Failed to open snd sysfs node, will retry for %d times ...", (retries - 1));
         } else {
             memset(buf , 0 ,sizeof(buf));
             lseek(fd,0L,SEEK_SET);
@@ -1059,7 +1059,7 @@ static int wait_for_snd_card_to_online()
             sscanf(buf , "%d", &card_status);
 
             if (card_status == SND_CARD_STATUS_ONLINE) {
-                AGM_LOGV(LOG_TAG, "snd sysfs node open successful");
+                AGM_LOGV("snd sysfs node open successful");
                 break;
             }
         }
@@ -1068,7 +1068,7 @@ static int wait_for_snd_card_to_online()
     } while ( retries > 0);
 
     if (0 == retries) {
-        AGM_LOGE(LOG_TAG, "Failed to open snd sysfs node, exiting ... ");
+        AGM_LOGE("Failed to open snd sysfs node, exiting ... ");
         ret = -EIO;
     }
 
