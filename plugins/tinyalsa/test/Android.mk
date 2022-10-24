e61ebdf1e9401b17a543a87e43ccb2804dbd283c
SGCMarkus <markusornik@gmail.com>
2022-10-24 16:59:42 +0200

    plugins: disable modules on OSS builds that depend on proprietary headers
    Change-Id: Ia67cffd7f8e10b6b2a413013ce05e8a98b4fa5d7

--- plugins/tinyalsa/test/Android.mk
+++ plugins/tinyalsa/test/Android.mk
@@ -1,4 +1,6 @@
 LOCAL_PATH := $(call my-dir)
+
+ifneq ($(QCPATH),)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        := libagmmixer
@@ -27,7 +29,9 @@ LOCAL_SHARED_LIBRARIES += \
      libexpat
 
 include $(BUILD_SHARED_LIBRARY)
+endif
 
+ifneq ($(QCPATH),)
 # Build agmplay
 include $(CLEAR_VARS)
 
@@ -57,6 +61,9 @@ LOCAL_SHARED_LIBRARIES += \
     libagmmixer
 
 include $(BUILD_EXECUTABLE)
+endif
+
+ifneq ($(QCPATH),)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        := agmcap
@@ -85,7 +92,9 @@ LOCAL_SHARED_LIBRARIES += \
     libagmmixer
 
 include $(BUILD_EXECUTABLE)
+endif
 
+ifneq ($(QCPATH),)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        := agmhostless
@@ -188,7 +197,9 @@ LOCAL_SHARED_LIBRARIES += \
     libagmmixer
 
 include $(BUILD_EXECUTABLE)
+endif
 
+ifneq ($(QCPATH),)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        := agmvoiceui
@@ -217,3 +228,4 @@ LOCAL_SHARED_LIBRARIES += \
     libagmmixer
 
 include $(BUILD_EXECUTABLE)
+endif
