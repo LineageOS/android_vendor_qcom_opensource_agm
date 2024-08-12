f4b4f65e72d2a7159311681f169b0da6eb045ebc
Arian <arian.kulmer@web.de>
2024-08-12 14:19:32 +0200

    Disable 32 bit variants for 64 bit audio
    Change-Id: Id3b3ea0dae64f59619cad3e793c2d1db7bc3dbb4

--- ipc/HwBinders/agm_ipc_service/Android.mk
+++ ipc/HwBinders/agm_ipc_service/Android.mk
@@ -5,6 +5,10 @@ LOCAL_MODULE        := vendor.qti.hardware.AGMIPC@1.0-impl
 LOCAL_MODULE_OWNER  := qti
 LOCAL_VENDOR_MODULE := true
 
+ifeq ($(SOONG_CONFIG_android_hardware_audio_run_64bit), true)
+LOCAL_MULTILIB := 64
+endif
+
 LOCAL_CFLAGS        += -v -Wall
 LOCAL_CFLAGS        += -D_ANDROID_
 LOCAL_SRC_FILES     := src/agm_server_wrapper.cpp
