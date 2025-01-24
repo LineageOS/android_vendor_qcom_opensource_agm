f9a8d177ecf31133082bc4f27672b7760e5d35ed
Michael Bestas <mkbestas@lineageos.org>
2025-01-24 10:05:04 +0200

    Revert "AGMIPC@1.0-impl/libagm: exclude from OSS builds"
    This reverts commit 8db5a2a568ebb44aec91e31742c92148010f1f0c.
    
    Change-Id: I623c58b619a811fc39e345664eb58bd3771ff51e

--- service/Android.mk
+++ service/Android.mk
@@ -7,7 +7,6 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc/public
 include $(BUILD_HEADER_LIBRARY)
 
 # Build libagm
-ifneq ($(QCPATH),)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        := libagm
@@ -70,6 +69,4 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DEVICE_PREPARE_SEQ)), true)
 endif
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
 
