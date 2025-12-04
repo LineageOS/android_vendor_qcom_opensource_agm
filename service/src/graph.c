111294f90cf8de248fb5576b66031b755e3025d0
Aravind Siddappaji <asiddapp@qti.qualcomm.com>
2025-12-04 21:01:08 +0530

    agm: Fix static analysis issues & warnings
    Fix static analysis issues & compiler warnings
    
    Change-Id: Ibf6a71be777ccc8a09966028eed55fc278c40110
    Signed-off-by: Aravind Siddappaji <asiddapp@qti.qualcomm.com>

--- service/src/graph.c
+++ service/src/graph.c
@@ -26,8 +26,9 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
-** Changes from Qualcomm Innovation Center are provided under the following license:
-** Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *  SPDX-License-Identifier: BSD-3-Clause-Clear
 **
 ** Redistribution and use in source and binary forms, with or without
 ** modification, are permitted (subject to the limitations in the
@@ -58,7 +59,12 @@
 ** IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 ** OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 ** IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+ *  Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *  SPDX-License-Identifier: BSD-3-Clause-Clear
  */
+
 #define LOG_TAG "AGM: graph"
 
 #include <errno.h>
@@ -2222,7 +2228,7 @@ int graph_get_tckv_data_from_acdb(
         goto end;
     }
 
-    AGM_LOGI("expected size = %d query_size= %d",
+    AGM_LOGI("expected size = %zu query_size= %zu",
                 *payload_size, query_payload_size);
     if (*payload_size > query_payload_size) {
         AGM_LOGE("payload size is too small to hold result.");
