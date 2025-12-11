482a98bbe643505caf4952db32cabeb4da76d3de
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 09:51:21 +0200

    agm: Fix format specifiers
    Change-Id: Ia7b15629eb0a15bac4158c28765ebced92293cf0

--- ipc/aidl/aidlconverter/inc/agm/Utils.h
+++ ipc/aidl/aidlconverter/inc/agm/Utils.h
@@ -16,7 +16,8 @@
 #define RETURN_IF_KVPAIR_EXCEEDS_RANGE(pair)                              \
     ({                                                                    \
         if (pair > MAX_KVPAIR) {                                          \
-            ALOGE("Num KVs %d more than expected: %d", pair, MAX_KVPAIR); \
+            ALOGE("Num KVs %zu more than expected: %d",                   \
+                (size_t)pair, MAX_KVPAIR);                                \
             return status_tToBinderResult(-ENOMEM);                       \
         }                                                                 \
     })
