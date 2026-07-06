0e51688394476529fcc7bf311398792d4c4e2d39
Alexander Koskovich <akoskovich@pm.me>
2026-07-06 17:22:12 -0400

    agm: Import Awinic changes for aw882xx smart PAs
    These changes were ported from the v2.0.11 driver package Awinic
    publishes for the AW88271CSR.
    
    Change-Id: I82e466e4af1ec955dffab61348873b51d02dc68c

--- service/src/session_obj.c
+++ service/src/session_obj.c
@@ -36,6 +36,9 @@
 #include <string.h>
 #include <agm/session_obj.h>
 #include <agm/utils.h>
+#ifdef AGM_SUPPORT_AW882XX
+#include "apm_api.h"
+#endif
 
 #ifdef DYNAMIC_LOG_ENABLED
 #include <log_xml_parser.h>
@@ -1524,6 +1527,24 @@ int session_obj_set_sess_aif_params(struct session_obj *sess_obj,
        aif_obj->params = NULL;
        aif_obj->params_size = 0;
    }
+#ifdef AGM_SUPPORT_AW882XX
+   else {
+       struct apm_module_param_data_t *aw_data =
+                       (struct apm_module_param_data_t *)payload;
+
+       if (aw_data->param_id == 0x10013D2A ||
+           aw_data->param_id == 0x10013D2B ||
+           aw_data->param_id == 0x10013D36 ||
+           aw_data->param_id == 0x10013D33 ||
+           aw_data->param_id == 0x10013D2E ||
+           aw_data->param_id == 0x10013D11 ||
+           aw_data->param_id == 0x10013D12) {
+           free(aif_obj->params);
+           aif_obj->params = NULL;
+           aif_obj->params_size = 0;
+       }
+   }
+#endif
 
 done:
     pthread_mutex_unlock(&sess_obj->lock);
